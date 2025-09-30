ed81eb633a70531dd99e2a0e937f54afb45f6d05
Adewale Adesanya <adesanyaadewale89@gmail.com>
2025-09-30 10:13:33 -0700

    Added the homapage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,46 @@
 export default function Sidebar() {
   return (
-    <>
-      <div></div>
-    </>
+    <footer className="bg-pri w-full">
+      <div className="max-w-[1100px] mx-auto py-15 px-5 flex justify-between gap-10 md:flex-row flex-col">
+        <div className="w-full md:w-1/3">
+          <h4 className="text-white mb-4 font-semibold text-xl">Contact Us</h4>
+          <p className="text-light text-sm flex gap-3 items-center">
+            {/* Phone Icon */}
+            <i className="fas fa-phone"></i>08123377800
+          </p>
+        </div>
+        <ul className="list-none w-full md:w-1/3 space-y-4">
+          <li className="text-light text-sm flex gap-4 items-center">
+            <a href="">
+              {/* Instargram Icon */}
+              <i className="fab fa-instagram-f"></i>
+              @FarmMarket
+            </a>
+          </li>
+          <li className="text-light text-sm flex gap-4 items-center">
+            <a href="">
+              {/* X Icon */}
+              <i className="fab fa-twitter-f"></i>
+              @FarmMarketNg
+            </a>
+          </li>
+          <li className="text-light text-sm flex gap-4 items-center">
+            <a href="">
+              {/* Facebook Icon */}
+              <i className="fab fa-facebook-f"></i>
+              Farm Market Nigeria
+            </a>
+          </li>
+        </ul>
+        <div className="w-full md:w-1/3">
+          <h4 className="text-white mb-4 text-xl font-semibold">Address</h4>
+          <p className="text-light text-sm flex gap-4 items-center">
+            {/* Marker Icon */}
+            <i className="fas fa-phone"></i>Lorem ipsum dolor sit amet
+            consectetur. Faucibus aliquet laoreet rutrum id.
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 }
